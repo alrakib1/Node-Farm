4c75bb2afe1d2848d047e4d1fb3fa6812c29d382
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-01-19 00:35:28 +0600

    added template on overview

--- index.js
+++ index.js
@@ -5,18 +5,42 @@ const url = require("url");
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObject = JSON.parse(data);
 
+const templateOverview = fs.readFileSync(
+  `${__dirname}/templates/overview.html`,
+  "utf-8"
+);
+const templateCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const templateProduct = fs.readFileSync(
+  `${__dirname}/templates/product.html`,
+  "utf-8"
+);
+
 const server = http.createServer((req, res) => {
   //   console.log(req.url);
   const pathname = req.url;
+
+  // Overview page
   if (pathname == "/" || pathname === "/overview") {
-    res.end("this is overview ");
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    res.end(templateOverview);
+
+    // Product page
   } else if (pathname === "/product") {
     res.end("this is product");
+
+    // Api
   } else if (pathname === "/api") {
     res.writeHead(200, {
       "content-type": "application/json",
     });
     res.end(data);
+
+    // Not found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",

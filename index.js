8b4ba15106ce8159037592a3c60c03d1b937cfc4
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-01-19 00:08:21 +0600

    added sync file read

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObject = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   //   console.log(req.url);
   const pathname = req.url;
@@ -10,14 +13,10 @@ const server = http.createServer((req, res) => {
   } else if (pathname === "/product") {
     res.end("this is product");
   } else if (pathname === "/api") {
-    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
-      const productData = JSON.parse(data);
-      // console.log(productData);
-      res.writeHead(200, {
-        "content-type": "application/json",
-      });
-      res.end(data);
+    res.writeHead(200, {
+      "content-type": "application/json",
     });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",

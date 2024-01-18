7472b47cefd03ad69c56810a3be760c024cdc2db
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-01-19 00:00:16 +0600

    connected with json file

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const http = require("http");
+const fs = require("fs");
 const url = require("url");
 
 const server = http.createServer((req, res) => {
-//   console.log(req.url);
+  //   console.log(req.url);
   const pathname = req.url;
   if (pathname == "/" || pathname === "/overview") {
     res.end("this is overview ");
   } else if (pathname === "/product") {
     res.end("this is product");
+  } else if (pathname === "/api") {
+    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
+      const productData = JSON.parse(data);
+      // console.log(productData);
+      res.writeHead(200, {
+        "content-type": "application/json",
+      });
+      res.end(data);
+    });
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
     });
-    res.end('<h1>404 Page not found !!!</h1>');
+    res.end("<h1>404 Page not found !!!</h1>");
   }
 });
 
